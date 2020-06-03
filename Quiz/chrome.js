5d68b731709f26a93849b26f52758d358427a297
ProjectX-v0 <cloudmail.project.x@gmail.com>
2020-06-03 19:57:21 +0530

    New Website

---
+++ Quiz/chrome.js
@@ -0,0 +1,9 @@
+var isChrome =
+  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
+if (isChrome == false) {
+  alert(
+    " You are not using Chrome browser. Please use Chrome browser to download Certificate"
+  );
+}
+console.log(isChrome);
