e3c127ec99a7a1846c1a82e75f96e022c5b0d15e
ProjectX-v0 <cloudmail.project.x@gmail.com>
2020-06-05 11:08:23 +0530

    New Web

--- Quiz/chrome.js
+++
@@ -1,9 +0,0 @@
-var isChrome =
-  /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-
-if (isChrome == false) {
-  alert(
-    " You are not using Chrome browser. Please use Chrome browser for better experience"
-  );
-}
-console.log(isChrome);
